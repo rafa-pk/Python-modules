3ca8000bee38e18a45aa569fb9ec33ef5415c3aa
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2025-12-09 22:43:40 +0100

    Add files via upload

--- module00/ex7/ft_seed_inventory.py
+++ module00/ex7/ft_seed_inventory.py
@@ -1,4 +1,11 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
 
-
-    
+    if unit == "packets":
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} available")
+    elif unit == "grams":
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} total")
+    elif unit == "area":
+        print(f"{seed_type.capitalize()} seeds: "
+              f"covers {quantity} square meters")
+    else:
+        print("Unkown unit type")
