3f02f5dd8dd44eedb901d02c1afc35c4752e4047
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2025-12-17 12:46:26 +0100

    Add files via upload

--- module01/ex5/ft_plant_types.py
+++ module01/ex5/ft_plant_types.py
@@ -6,7 +6,7 @@ class Plant:
         self.age = age
 
 
-class Flower:
+class Flower(Plant):
 
     def __init__(self, name, height, age, color):
         super().__init__(name, height, age)
@@ -16,32 +16,64 @@ class Flower:
        print(f"{self.name} is blooming beautifully!") 
 
 
-class Tree:
+class Tree(Plant):
 
     def __init__(self, name, height, age, trunk_diameter):
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
 
     def produce_shade(self):
-        print(f"{self.name} produces {2 * 3.14 ** ({trunk_diameter} / 10)}
-              square meters")
+        radius = self.trunk_diameter / 10
+        print(f"{self.name} provides "
+              f"{(3.14 * (radius ** 2)):.0f} square meters")
 
 
-class Vegetable:
+class Vegetable(Plant):
 
     def __init__(self, name, height, age, harvest_season, nutritional_value):
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
         self.nutritional_value = nutritional_value
 
+    def nutritional_v(self):
+        print(f"{self.name} is rich in {self.nutritional_value}")
 
+ 
 def ft_plant_types():
 
     rose = Flower("Rose", 25, 30, "red")
+    marguerite = Flower("Marguerite", 15, 10, "white")
     oak = Tree("Oak", 500, 1825, 50)
-    tomato = Vegetable("Tomato", 80, 90, "summer harvest", "vitamin C")
+    bush = Tree("Bush", 100, 928383, 800)
+    tomato = Vegetable("Tomato", 80, 90, "summer", "vitamin C")
+    avocado = Vegetable("Avocado", 50, 80, "summer", "bons lipides")
     
     print("=== Garden Plant Types ===")
-    print(f"")
+    print(f"\n{rose.name} ({rose.__class__.__name__}): {rose.height}cm, "
+          f"{rose.age} days {rose.color} color")
+    rose.bloom()
+    print(f"\n{marguerite.name} ({marguerite.__class__.__name__}): "
+        f"{marguerite.height}cm, {marguerite.age} days {marguerite.color} "
+        f"color")
+    marguerite.bloom()
+    print(f"\n{oak.name} ({oak.__class__.__name__}): {oak.height}cm, {oak.age}"
+          f" days, {oak.trunk_diameter}cm diameter")
+    oak.produce_shade()
+    print(f"\n{bush.name} ({bush.__class__.__name__}): {bush.height}cm, "
+           f"{bush.age} days, {bush.trunk_diameter}cm diameter")
+    bush.produce_shade()
+    print(f"\n{tomato.name} ({tomato.__class__.__name__}): {tomato.height}cm,"
+          f" {tomato.age} days, {tomato.harvest_season} harvest")
+    tomato.nutritional_v()
+    print(f"\n{avocado.name} ({avocado.__class__.__name__}): "
+        f"{avocado.height}cm, {avocado.age} days, {avocado.harvest_season}"
+        f" harvest")
+    avocado.nutritional_v()
 
 
+def main():
+    ft_plant_types()
+
+
+if __name__ == "__main__":
+    main()
---
+++ module01/ex6/ft_garden_analytics.py
@@ -0,0 +1,16 @@
+class GardenManager:
+
+    def __init__()
+
+    def create_garden_network(self):
+
+
+class GardenStats(GardenManager):
+
+
+
+
+def ft_garden_analytics():
+
+
+    print("=== Garden Management System Demo ===")
