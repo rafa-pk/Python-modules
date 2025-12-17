c0f2356b61aa44d89edd11927f06ab65392efcb8
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2025-12-17 13:34:42 +0100

    Add files via upload

--- module01/ex6/ft_garden_analytics.py
+++ module01/ex6/ft_garden_analytics.py
@@ -1,11 +1,29 @@
-class GardenManager:
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
 
-    def __init__()
+
+class FloweringPlant(Plant)
+    def __init__(self, name, height, age, color, bloom_state):
+        super().__init__(name, height, age)
+        self.color = color
+        self.bloom_state = bloom_state
+
+class PrizeFlower(FloweringPlant):
+    def __init__(self, name, height, age, color, bloom_state, scoring):
+        super().__init__(name, height, age, color, bloom_state)
+        self.scoring = scoring
+
+class GardenManager:
+    def __init__(self):
 
     def create_garden_network(self):
+        
 
-
-class GardenStats(GardenManager):
+class GardenStats():
+    def __init__(self):
 
 
 
