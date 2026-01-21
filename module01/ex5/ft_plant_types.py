568fb27ae7573b782c9813653665d0c3c28eb5c6
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2026-01-21 13:11:24 +0100

    Add files via upload

--- module01/ex5/ft_plant_types.py
+++ module01/ex5/ft_plant_types.py
@@ -1,44 +1,54 @@
 class Plant:
-    """Plant blueprint, parent class, defines plant object with base characteristics"""
+    """Plant blueprint, parent class, defines plant object with
+    base characteristics"""
     def __init__(self, name: str, height: int, age: int) -> None:
-        """initalization method, associates base characteristics to new object"""
+        """initalization method, associates base characteristics
+        to new object"""
         self.name = name
         self.height = height
         self.age = age
 
 
-class Flower(Plant) -> None:
-    """Flower blueprint, inherits from Plant but adds a color characteristic"""
+class Flower(Plant):
+    """Flower blueprint, inherits from Plant but adds a color
+    characteristic"""
     def __init__(self, name: str, height: int, age: int, color: str) -> None:
-        """initalization method, associates characteristics of parent class
-        and Flower class to object"""
+        """initalization method, associates characteristics of
+        parent class and Flower class to object"""
         super().__init__(name, height, age)
         self.color = color
 
     def bloom(self) -> None:
-        """method which prints the name of a flower object blooming well"""
-       print(f"{self.name} is blooming beautifully!") 
+        """method which prints the name of a flower object
+        blooming well"""
+        print(f"{self.name} is blooming beautifully!")
 
 
-class Tree(Plant) -> None:
-    """Tree blueprint, inherits from Plant and adds a trunk diameter characteristic"""
-    def __init__(self, name: str, height: int, age: int, trunk_diameter: int) -> None:
-        """initialization method, associates characteristics of parent class
-        and Tree class to object"""
+class Tree(Plant):
+    """Tree blueprint, inherits from Plant and adds a trunk
+    diameter characteristic"""
+    def __init__(self, name: str, height: int, age: int,
+                 trunk_diameter: int) -> None:
+        """initialization method, associates characteristics of
+        parent class and Tree class to object"""
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
 
     def produce_shade(self) -> None:
-        """method which calculates the area of produced shade by the Tree and prints it"""
+        """method which calculates the area of produced shade by the Tree
+        and prints it"""
         radius = self.trunk_diameter / 10
         print(f"{self.name} provides "
               f"{(3.14 * (radius ** 2)):.0f} square meters")
 
 
-class Vegetable(Plant) -> None:
-    """Vegetable blueprint, inherits from Plan and adds harvest season and nutritional value characteristics"""
-    def __init__(self, name: str, height: int, age: int, harvest_season: str, nutritional_value: str) -> None:
-        """initalizer method, associates characteristics of parent class and Vegetable method to object"""
+class Vegetable(Plant):
+    """Vegetable blueprint, inherits from Plan and adds harvest season
+    and nutritional value characteristics"""
+    def __init__(self, name: str, height: int, age: int, harvest_season: str,
+                 nutritional_value: str) -> None:
+        """initalizer method, associates characteristics of parent class and
+        Vegetable method to object"""
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
         self.nutritional_value = nutritional_value
@@ -47,40 +57,42 @@ class Vegetable(Plant) -> None:
         """method which prints nutritional value to stdout"""
         print(f"{self.name} is rich in {self.nutritional_value}")
 
- 
+
 def ft_plant_types() -> None:
-    """"""
+    """function which initializes instances of multiple objects
+    inheriting from Plant and prints their characteristics"""
     rose = Flower("Rose", 25, 30, "red")
     marguerite = Flower("Marguerite", 15, 10, "white")
     oak = Tree("Oak", 500, 1825, 50)
     bush = Tree("Bush", 100, 928383, 800)
     tomato = Vegetable("Tomato", 80, 90, "summer", "vitamin C")
     avocado = Vegetable("Avocado", 50, 80, "summer", "bons lipides")
-    
+
     print("=== Garden Plant Types ===")
     print(f"\n{rose.name} ({rose.__class__.__name__}): {rose.height}cm, "
           f"{rose.age} days {rose.color} color")
     rose.bloom()
     print(f"\n{marguerite.name} ({marguerite.__class__.__name__}): "
-        f"{marguerite.height}cm, {marguerite.age} days {marguerite.color} "
-        f"color")
+          f"{marguerite.height}cm, {marguerite.age} days {marguerite.color} "
+          f"color")
     marguerite.bloom()
     print(f"\n{oak.name} ({oak.__class__.__name__}): {oak.height}cm, {oak.age}"
           f" days, {oak.trunk_diameter}cm diameter")
     oak.produce_shade()
     print(f"\n{bush.name} ({bush.__class__.__name__}): {bush.height}cm, "
-           f"{bush.age} days, {bush.trunk_diameter}cm diameter")
+          f"{bush.age} days, {bush.trunk_diameter}cm diameter")
     bush.produce_shade()
     print(f"\n{tomato.name} ({tomato.__class__.__name__}): {tomato.height}cm,"
           f" {tomato.age} days, {tomato.harvest_season} harvest")
     tomato.nutritional_v()
     print(f"\n{avocado.name} ({avocado.__class__.__name__}): "
-        f"{avocado.height}cm, {avocado.age} days, {avocado.harvest_season}"
-        f" harvest")
+          f"{avocado.height}cm, {avocado.age} days, {avocado.harvest_season}"
+          f" harvest")
     avocado.nutritional_v()
 
 
 def main():
+    """main function, called in entry point"""
     ft_plant_types()
 
 
--- module01/ex6/ft_garden_analytics.py
+++ module01/ex6/ft_garden_analytics.py
@@ -1,39 +1,76 @@
 class Plant:
-    def __init__(self, name, height, age):
+    """parent class, blueprint for an arbitrary plant object"""
+    def __init__(self, name: str, height: int) -> None:
+        """initialization method for Plant class"""
         self.name = name
         self.height = height
         self.age = age
 
-    def grow(self, height)
-        print(f"{self.name} grew {height - self.height}cm")
+    def grow(self, x: int) -> None:
+        """general instance method, which makes a plant grow by x cm and
+        prints by how much it has grown"""
+        self.height += x
+        print(f"{self.name} grew {x}cm\n")
+
+    def get_info(self) -> str:
+        """general instance method which returns the plants info"""
+        return f"{self.name}: {self.height}cm"
 
 
-class FloweringPlant(Plant)
-    def __init__(self, name, height, age, color, bloom_state):
-        super().__init__(name, height, age)
+class FloweringPlant(Plant):
+    """child of Plant class, blueprint for a flowering plant"""
+    def __init__(self, name: str, height: int, color: str) -> None:
+        """initialization method for FloweringPlant child class"""
+        super().__init__(name, height)
         self.color = color
-        self.bloom_state = bloom_state
+        self.status = True
+
+
+    def get_info(self):
+        print(f"{super().get_info()}, {self.color} flowers (blooming)\n")
+
 
 class PrizeFlower(FloweringPlant):
-    def __init__(self, name, height, age, color, bloom_state, scoring):
-        super().__init__(name, height, age, color, bloom_state)
-        self.scoring = scoring
+    """child of FloweringPlant, blueprint for a prize flower"""
+    def __init__(self, name: str, height: int, color: str, points: int) -> None:
+        """initialization method for PrizeFlower child class"""
+        super().__init__(self, name, height, color)
+        self.points = points
 
-    def 
 class GardenManager:
-    def __init__(self):
-
-    def create_garden_network(self):
+    """manager class, which will manage multiple gardens"""
+    def __init__(self, garden_name):
         
-
-class GardenStats():
-    def __init__(self):
-        self.gardens = {}
-
+    
+    class GardenStats:
+        def __init__(self)
 
 
+    @classmethod
+    def create_garden_network(cls)
 
-def ft_garden_analytics():
 
+def ft_garden_analytics() -> None:
+    """general function where multiple methods are called
+    from multiple classes, is called from entry point"""
+    alice_garden = GardenManager("Alice")
+    bob_garden = GardenManager("Bob")
+    oak_tree = Plant("Oak Tree, 100")
+    rose = FloweringPlant("Rose", 26, red)
+    sunflower = PrizeFlower("Sunflower", 50, yellow, 10)
 
-    print("=== Garden Management System Demo ===")
+    print("=== Garden Management System Demo")
+    alice_garden.add_plant(oak_tree)
+    alice_garden.add_plant(rose)
+    alice_garden.add_plant(sunflower)
+    
+    alice_garden.help_all()
+    alice_garden.report()
+    print(f"Height validation test: {Plant.val_height()}")
+    GardenManager.compare_gardens(alice_garden, bob_garden)
+    print(f"Total gardens managed: {GardenManager.get_total_gardens()}\n")
+    
+
+if __name__ == "__main__":
+    """program's entry point, calls general function"""
+    ft_garden_analytics()
