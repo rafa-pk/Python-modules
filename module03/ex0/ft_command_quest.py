bb37834ee0dbc8490ce29ca0f2cf37e07735e7c9
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2026-01-24 13:17:13 +0100

    Add files via upload

---
+++ module03/ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+import sys
+
+def main() -> None:
+    print("=== Command Quest ===")
+    ac = len(sys.argv)
+    if ac == 1:
+        print("No arguments provided!")
+        print(f"Program name: {sys.argv[0]}")
+    else:
+        print(f"Program name: {sys.argv[0]}")
+        print(f"Arguments received: {ac - 1}")
+        for i in range(1, ac):
+            print(f"Argument {i}: {sys.argv[i]}")
+    print(f"Total arguments: {ac}")
+
+if __name__ == "__main__":
+    main()
