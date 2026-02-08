cf392a9e8c04c234662fc4786681c73a3b1af3fe
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2026-02-08 09:58:04 +0100

    Add files via upload

--- module03/ex3/ft_achievement_tracker.py
+++ module03/ex3/ft_achievement_tracker.py
@@ -1,5 +1,18 @@
+class Player:
+    """Class meant to represent a player and its achievements"""
+    def __init__(self, name: str) -> None:
+        """initialization method"""
+        self.name = name
+        self.achivements = set()
+
+
 def main() -> None
+    
+
     print("=== Achievement Tracker System ===")
+    player.display_achievements()
+
+    print("=== Achievement analytics ===")
 
 
 
